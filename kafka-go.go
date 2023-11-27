0f5b3f02c3bf00659c5b0719f511f652467590f2
Jade <85654561+jp524@users.noreply.github.com>
2023-11-27 11:44:05 -0500

    Build producer for Kafka

--- kafka-go.go
+++ kafka-go.go
@@ -2,9 +2,87 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 	"github.com/IBM/sarama"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("Hello, world.")
+	app := fiber.New()
+	app.Post("/messages", createMessage)
+	app.Listen(":3000")
 }
+
+// Producer
+func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
+	conf := sarama.NewConfig()
+	conf.Producer.Return.Successes = true
+	conf.Producer.RequiredAcks = sarama.WaitForAll
+	conf.Producer.Retry.Max = 5
+
+	conn, err := sarama.NewSyncProducer(brokersUrl, conf)
+
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func PushCommentToQueue(topic string, message []byte) error {
+	brokersUrl := []string{"localhost:9092"}
+	producer, err := ConnectProducer(brokersUrl)
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
+	return nil
+}
+
+type Message struct {
+	Text string `form:"text" json:"text"`
+}
+
+func createMessage(c *fiber.Ctx) error {
+	// Instantiate new Message struct
+	msg:= new(Message)
+
+	// Parse comment as body for HTTP request and return if error
+	if err := c.BodyParser(msg); err != nil {
+		c.Status(400).JSON(&fiber.Map{
+			"success": false,
+			"comment": err,
+		})
+		return err
+	}
+
+	// Convert body into bytes and send it to Kafka
+	msgInBytes, err := json.Marshal(msg)
+	PushCommentToQueue("messages", msgInBytes)
+
+	// Return Comment in JSON format
+	err = c.JSON(&fiber.Map{
+		"success": true,
+		"comment": "Message pushed successfully",
+		"message": msg,
+	})
+	if err != nil {
+		c.Status(500).JSON(&fiber.Map{
+			"success": false,
+			"comment": "Error creating product",
+		})
+		return err	
+	}
+	return err
+}
+// End of Producer
